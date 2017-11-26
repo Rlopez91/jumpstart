2e8d86925c437b9685f02b9988c989a99339c1cf
Rick L <richyray24@gmail.com>
2017-11-26 15:01:16 -0500

    finished work with javascript

--- resources/jquery/main.js
+++ resources/jquery/main.js
@@ -1,15 +1,17 @@
-$(document).ready(() => {
-  $(window).scroll(function() {
- let scrolledDown = 0;
- let topNav = $(this).scrollTop();
+$(document).ready(function(){
+  var scrollDown = 0
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
 
- if(topNav - scrolledDown > 50){
-   let navHeight = $('.navbar').css('height');
-   $('.navbar').animate({top: '-' + navHeight}, 150);
- }
+    if(scrollTop - scrollDown > 50){
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollDown = scrollTop;
+    }
 
- else if (scrolledDown + 50 > topNav){
-   $('.navbar').animate({top: 0}, 150);
- }
+    else if (scrollDown - scrollTop > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollDown = scrollTop;
+    }
+  });
 });
-})
